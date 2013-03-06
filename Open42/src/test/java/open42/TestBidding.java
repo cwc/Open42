9e1b08d3d82b301b55ded4e1ff925174b8951a91
Cameron Currie <me@cameroncurrie.net>
2013-03-06 13:43:55 -0600

    Add Maven/Travis configuration
    Also renamed bidding test file so it's recognized by Maven with default
    settings.

--- Open42/src/test/java/open42/TestBidding.java
+++ Open42/src/test/java/open42/TestBidding.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-public class BidTests {
+public class TestBidding {
 	private Hand testHand;
 
 	@Before
